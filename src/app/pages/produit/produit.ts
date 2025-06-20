a6f8728eebeb3dfb42c85218c9b5d8a998b9eb22
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2025-06-20 12:35:58 +0200

    fix(build): pre render route

--- src/app/pages/produit/produit.ts
+++ src/app/pages/produit/produit.ts
@@ -1,12 +1,8 @@
-import { ChangeDetectorRef, Component, inject, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../services/requests/product';
 import { HeroProductComponent } from '../../components/hero/product-hero';
-import { CommonModule, isPlatformServer } from '@angular/common';
-import { TransferState } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
-
-const PRODUCT_KEY_PREFIX = 'product-data-';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-produit',
@@ -19,64 +15,28 @@ export class Produit implements OnInit {
   page: any = null;
   error: string | null = null;
   slug = '';
-  loading = true;
-
   private productService = inject(ProductService);
   private cdr = inject(ChangeDetectorRef);
-  private route = inject(ActivatedRoute);
-  private transferState = inject(TransferState);
-  private platformId = inject(PLATFORM_ID);
 
-  constructor() {
-    if (isPlatformServer(this.platformId)) {
-      this.initializeData();
-    }
-  }
-
-  async initializeData(): Promise<void> {
-    const params = await firstValueFrom(this.route.params);
-    this.slug = params['slug'] ?? '';
-    const key = PRODUCT_KEY_PREFIX + this.slug;
-
-    try {
-      const data = await this.productService.getProductAndMoreProducts(this.slug, false, null);
-      this.page = data?.product;
-      this.transferState.set(key as any, this.page);
-      this.loading = false;
-    } catch (e) {
-      this.error = 'Erreur de chargement';
-      this.loading = false;
-    }
-  }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.slug = params['slug'] ?? '';
-      const key = PRODUCT_KEY_PREFIX + this.slug;
-
-      if (this.transferState.hasKey(key as any)) {
-        this.page = this.transferState.get(key as any, null);
-        this.loading = false;
-        this.transferState.remove(key as any);
-        this.cdr.detectChanges();
-      } else {
-        this.loadPage();
-      }
+    this.route.data.subscribe(data => {
+      this.page = data['productData']?.product;
+      this.cdr.detectChanges();
     });
   }
 
   loadPage() {
-    this.loading = true;
     this.productService.getProductAndMoreProducts(this.slug, false, null)
       .then(data => {
         this.page = data?.product;
-        this.loading = false;
         this.cdr.detectChanges();
       })
       .catch(e => {
         this.error = 'Erreur de chargement';
-        this.loading = false;
         this.cdr.detectChanges();
+        console.error(e);
       });
   }
 }
---
+++ src/app/services/resolvers/product.resolver.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { ProductService } from '../../services/requests/product';
+
+@Injectable({ providedIn: 'root' })
+export class ProductResolver implements Resolve<any> {
+  constructor(private productService: ProductService) { }
+
+  resolve(route: ActivatedRouteSnapshot) {
+    const slug = route.paramMap.get('slug')!;
+    console.log('Resolver called for slug:', slug);
+    return this.productService.getProductAndMoreProducts(slug, false, null);
+  }
+}
